5f347d93ea537120d0ccfe6ecf3abe29af8d35a5
Juanma <juanaco1.p@gmail.com>
2021-05-26 18:24:45 +0200

    binance module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Binance from 'binance-api-node';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  client: any;
+
+  constructor(private configService: ConfigService) {
+    this.client = Binance({
+      apiKey: this.configService.get<string>('BINANCE_KEY'),
+      apiSecret: this.configService.get<string>('BINANCE_SECRET'),
+    });
+  }
+
+  async getBalance(): Promise<number> {
+    const res = await this.client.accountInfo();
+
+    return parseFloat(res.balances.find(b => b.asset === 'EUR').free);
+  }
+
+  async sendOrder(): Promise<any> {
+    return await this.client.order({
+      symbol: 'BTCEUR',
+      side: 'BUY',
+      type: 'MARKET',
+      quoteOrderQty: '10',
+    });
   }
 }
